c014479a50f1cdcc82a883e3d52205dd330dc164
WillSWiggins <144835338+WillSWiggins@users.noreply.github.com>
2023-09-12 12:51:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = (process.env.PORT || 3000)
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let myVariableServer = 'soft coded server data'
+
+app.get('/views', (req, res) => {
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer 
+  }
+  );
+})
+
+app.post('/postClientData', function (req, res) {
+  
+  console.log("body: ", req.body)
+  console.log("user Name: ", req.body.userName)
+ //  console.log("params: ", req.params['userName']);
+ 
+ // myVariableServer = req.body.userName;
+
+ res.render('index', 
+ {
+   'myVariableClient' : req.body.userName 
+ }
+ );
+})
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World From Express & a PaaS/Render</h1><br><a href="/views">November Lab');
+})
+
+app.get('/whatever', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+
+// app.listen(3000)
+
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
